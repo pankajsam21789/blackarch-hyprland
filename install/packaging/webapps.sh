c59c7ca2b0a7e3d9245fdfcfc19f946fa04cdfb0
Pankaj Singh <168477091+pankajsam21789@users.noreply.github.com>
2025-12-30 10:22:39 +0530

    added hacking labs on virtualbox

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,5 +1,3 @@
-blackarch-webapp-install "HEY" https://app.hey.com HEY.png "blackarch-webapp-handler-hey %u" "x-scheme-handler/mailto"
-blackarch-webapp-install "Basecamp" https://launchpad.37signals.com Basecamp.png
 blackarch-webapp-install "WhatsApp" https://web.whatsapp.com/ WhatsApp.png
 blackarch-webapp-install "Google Photos" https://photos.google.com/ "Google Photos.png"
 blackarch-webapp-install "Google Contacts" https://contacts.google.com/ "Google Contacts.png"
@@ -7,8 +5,7 @@ blackarch-webapp-install "Google Messages" https://messages.google.com/web/conve
 blackarch-webapp-install "ChatGPT" https://chatgpt.com/ ChatGPT.png
 blackarch-webapp-install "YouTube" https://youtube.com/ YouTube.png
 blackarch-webapp-install "GitHub" https://github.com/ GitHub.png
-blackarch-webapp-install "X" https://x.com/ X.png
-blackarch-webapp-install "Figma" https://figma.com/ Figma.png
 blackarch-webapp-install "Discord" https://discord.com/channels/@me Discord.png
 blackarch-webapp-install "Zoom" https://app.zoom.us/wc/home Zoom.png "blackarch-webapp-handler-zoom %u" "x-scheme-handler/zoommtg;x-scheme-handler/zoomus"
-blackarch-webapp-install "Fizzy" https://app.fizzy.do/ Fizzy.png
+blackarch-webapp-install "Metasploitable-2" https://docs.rapid7.com/metasploit/metasploitable-2/ X.png
+blackarch-webapp-install "Metasploitable-3" https://github.com/rapid7/metasploitable3/wiki X.png
